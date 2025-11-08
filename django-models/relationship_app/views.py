8fd7aaa8107fca19d37fca07c4f0e27c5ce3b261
Natnael Tesfahun <nattytesfa4@gmail.com>
2025-11-08 22:56:33 +0300

    Fix views.py imports to use 'from django.views.generic.detail import DetailView'

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Library
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView 
+from .models import Book, Library
 
+# Function-based view to list all books
 def list_books(request):
-    from .models import Book
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+# Class-based view to display library details
 class LibraryDetailView(DetailView):
-    from .models import Library
     model = Library
     template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+# Class-based view to list all libraries
+class LibraryListView(ListView):
+    model = Library
+    template_name = 'relationship_app/library_list.html'
+    context_object_name = 'libraries'
