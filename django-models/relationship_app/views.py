264151af2c73664f613ceda65b5d1a881e6882d2
Natnael Tesfahun <nattytesfa4@gmail.com>
2025-11-09 00:36:12 +0300

    Fix urls.py to include exact patterns: views.register, LoginView.as_view(), LogoutView.as_view() with template names

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,12 +1,38 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView  # Exact import pattern
-from .models import Library  # Library import at top level
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView
+from django.views.generic.detail import DetailView
+from .models import Book, Library
 
+# Function-based view to list all books
 def list_books(request):
-    from .models import Book
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
+# Class-based view to display library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+# Class-based view to list all libraries
+class LibraryListView(ListView):
+    model = Library
+    template_name = 'relationship_app/library_list.html'
+    context_object_name = 'libraries'
+
+# User registration view (this is what the checker wants as views.register)
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('relationship_app:list_books')
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+# We'll use class-based views for login/logout as specified in urls.py
