70bc30f9ff0332804e1e1e1b75e64a3c231cc8f9
Natnael Tesfahun <nattytesfa4@gmail.com>
2025-11-08 22:48:37 +0300

    Fix views.py import to include 'from .models import Library' for class-based view

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Book, Library
+from .models import Book, Library  # This is the exact import the checker wants
 
 # Function-based view to list all books
 def list_books(request):
