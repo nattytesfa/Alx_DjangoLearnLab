b270cf791560974cf909e2ee1dc5f47f5f50a42a
Natnael Tesfahun <nattytesfa4@gmail.com>
2025-11-08 23:10:04 +0300

    Fix urls.py import to include 'from .views import list_books'

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView, LibraryListView  
 
 app_name = 'relationship_app'
 
 urlpatterns = [
     # Function-based view: List all books
-    path('books/', views.list_books, name='list_books'),
+    path('books/', list_books, name='list_books'),
     
     # Class-based view: List all libraries
-    path('libraries/', views.LibraryListView.as_view(), name='library_list'),
+    path('libraries/', LibraryListView.as_view(), name='library_list'),
     
     # Class-based view: Detail view for a specific library
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
