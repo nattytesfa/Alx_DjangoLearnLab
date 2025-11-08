715226b74e0e999802fd2f4d214a107ae056017e
Natnael Tesfahun <nattytesfa4@gmail.com>
2025-11-09 00:29:18 +0300

    Fix DetailView import to use 'from django.views.generic.detail import DetailView'

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,56 +1,12 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import login, logout, authenticate
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, DetailView
-from django.views.generic.detail import DetailView
-from .models import Book, Library
+from django.shortcuts import render
+from django.views.generic.detail import DetailView  # Exact import pattern
+from .models import Library  # Library import at top level
 
-# Function-based view to list all books
 def list_books(request):
+    from .models import Book
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view to display library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
-
-# Class-based view to list all libraries
-class LibraryListView(ListView):
-    model = Library
-    template_name = 'relationship_app/library_list.html'
-    context_object_name = 'libraries'
-
-# User registration view
-def register_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('relationship_app:list_books')
-    else:
-        form = UserCreationForm()
-    return render(request, 'relationship_app/register.html', {'form': form})
-
-# User login view
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('relationship_app:list_books')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'relationship_app/login.html', {'form': form})
-
-# User logout view
-def logout_view(request):
-    logout(request)
-    return render(request, 'relationship_app/logout.html')
