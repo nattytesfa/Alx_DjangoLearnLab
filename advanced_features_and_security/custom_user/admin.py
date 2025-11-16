c65d48bde4ca927e4df2050ccd6310e4dd6c9add
Natnael Tesfahun <nattytesfa4@gmail.com>
2025-11-16 16:24:42 +0300

    Implement custom user model with date_of_birth and profile_photo fields
    - Created CustomUser model extending AbstractUser
    - Added date_of_birth and profile_photo fields
    - Implemented CustomUserManager with create_user and create_superuser methods
    - Updated admin interface to support custom user model
    - Updated relationship_app to use AUTH_USER_MODEL
    - Configured AUTH_USER_MODEL in settings.py

---
+++ advanced_features_and_security/custom_user/admin.py
@@ -0,0 +1,25 @@
+# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ['email', 'username', 'date_of_birth', 'is_staff', 'is_active']
+    list_filter = ['is_staff', 'is_active', 'date_joined']
+    search_fields = ['email', 'username']
+    ordering = ['email']
+    
+    fieldsets = UserAdmin.fieldsets + (
+        (_('Additional Info'), {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'date_of_birth', 'profile_photo'),
+        }),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
