717c542dac5dc445aa35c932a05d624954ab2480
Natnael Tesfahun <nattytesfa4@gmail.com>
2025-11-23 08:32:56 +0300

    Create Django API project with DRF and Book model

---
+++ api_project/api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
