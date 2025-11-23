efda2d87ee0d8ad64c8fcb295c8aa89b69fef234
Natnael Tesfahun <nattytesfa4@gmail.com>
2025-11-23 08:51:42 +0300

    Set up Book API endpoint with serializer, view, and URL configuration

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    """
+    API view to retrieve list of all books
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
